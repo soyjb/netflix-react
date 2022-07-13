4bfe24074d5a117a7b507bbe4b5ae7cd3e19ed04
soyjb <thejulianbeltran@gmail.com>
2022-07-13 17:47:51 -0400

    added my project

---
+++ src/context/AuthContext.js
@@ -0,0 +1,50 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth, db } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  updateCurrentUser,
+} from "firebase/auth";
+import {setDoc, doc} from 'firebase/firestore';
+
+const AuthContext = createContext();
+
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState({});
+
+  function signUp(email, password) {
+    createUserWithEmailAndPassword(auth, email, password);
+    setDoc(doc(db, 'users', email), {
+      savedShows: []
+    })
+  }
+
+  function logIn(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  });
+
+  function logOut() {
+    signOut(auth);
+  }
+
+  return (
+    <AuthContext.Provider value={{ signUp, logIn, logOut, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function UserAuth() {
+  return useContext(AuthContext);
+}
